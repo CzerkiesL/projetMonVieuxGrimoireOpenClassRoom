c262267282366e98162e3a40ff2b501e9d7576e4
CzerkiesL <h3llgate@outlook.fr>
2024-08-20 10:03:41 +0200

    authentification system

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,26 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+
+const userRoutes = require('./routes/user')
+const bookRoutes = require('./routes/book')
+
 const app = express()
 
-module.exports = app
-
 mongoose.connect("mongodb+srv://czerkiesludovic:aENu3mtt7w98xscr@monvieuxgrimoire.qjopu.mongodb.net/?retryWrites=true&w=majority&appName=monVieuxGrimoire")
-    .then(() => console.log('Connexion réussie !'))
-    .catch(() => console.log('Connexion échouée !'))
+.then(() => console.log('Connexion réussie !'))
+.catch(() => console.log('Connexion échouée !'))
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next()
+})
+
+app.use(bodyParser.json())
+
+app.use('/api/auth', userRoutes)
+app.use('/api/books', bookRoutes)
+
+module.exports = app
